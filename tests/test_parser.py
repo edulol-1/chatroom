10ec543541a3f84455fef894e400a7d5e8543439
edulol-1 <edu3000@ciencias.unam.mx>
2023-07-25 20:54:29 -0600

    Creation of the class test for the parser

---
+++ tests/test_parser.py
@@ -0,0 +1,10 @@
+import unittest
+from src.main.chatroom import run_parser
+
+class TestChatRoom(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
